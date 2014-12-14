44fa8ec90f5113e13f2f0b9de11d549c1a833938
Anton D. Kachalov <mouse@yandex.ru>
2014-12-15 00:38:09 +0200

    ...intermediate state...
    Signed-off-by: Anton D. Kachalov <mouse@yandex.ru>

--- lib/qb/__init__.py
+++ lib/qb/__init__.py
@@ -1,14 +1,44 @@
 import sys
+from types import MethodType
 from qb._core import qb_object_t, qb_interface_t
+from qb._map import qb_map_interface_t
 from qb import _memoryregion
+class my_object(qb_object_t):
+#    class _ifaces(qb_object_t):
+#        pass
+
+#    iface = _ifaces
+
+    #@iface_func('(O|O)')
+    def do_something(self, *objs):
+        pass
+
+    @classmethod
+    def register_iface(cls, name, fn):
+        print('DOREG {} {} {}'.format(cls, name, fn))
+        exec("cls.{} = fn".format(name))
 
 class my_iface(qb_interface_t):
     #@iface_func('(O|O)')
     def do_something(self, *objs):
         pass
 
+def testfn(self, value):
+    print('TESTFN {} {} {}'.format(self, value, self.obj))
+
 def main():
 #    from qb._memoryregion import MemoryRegion
+    my_object.iface.test = my_iface
+    qb_object_t.iface.fff = my_iface
+#    obj = my_object()
+#    obj2 = qb_object_t()
+    print(my_object.iface)
+#    print(my_iface, obj.ifc.test)
+#    print(obj.iface.test)
+#    print(obj2.iface.test)
+    sys.exit(0)
+
+
     m = _memoryregion.MemoryRegion()
     m2 = _memoryregion.MemoryRegion()
     obj = qb_object_t()
@@ -16,11 +46,29 @@ def main():
         print(obj.o.fff)
     except AttributeError as e:
         print(e)
-    obj.iface.test = qb_interface_t
+    print('get')
+#    print(qb_object_t.iface.register) #.fff2 = testfn
+    print('end')
+
+    print(obj.iface)
+    print(qb_object_t.iface)
+    print('OK')
+    #print(type(my_iface.iface))
+    print('DIC', obj.iface) #fff = my_iface
+#    print(qb_object_t.iface)
+    #print(obj.iface.fff)
+
+
+#    obj.iface.test = my_iface
+#    obj.iface.test.doreg('fff', testfn)
+#    obj.iface.test.fff = testfn
+#    obj.iface.test.fff('ha-ha')
     # or
-    obj.iface.foo = _memoryregion.MemoryRegion
+    o2 = my_object()
+    print(o2.iface, obj.iface) #.test)
+    obj.iface.foo = qb_map_interface_t
     print(issubclass(my_iface, qb_interface_t)) #.o.aaa = 555
-    print(obj.iface.test, obj.iface.foo)
+    print(obj.iface.foo, obj.iface.foo)
     obj.iface.ffff = qb_interface_t
     print(obj.iface.ffff)
     #print(dir(m2.o))
--- src/_core.c
+++ src/_core.c
@@ -25,6 +25,7 @@ typedef struct {
     PyObject_HEAD
     PyObject *parentobj;
     PyObject *iface;
+    PyObject *iface_defs;
 } qb_interface_set_t;
 
 PyTypeObject qb_object_type;
@@ -39,7 +40,7 @@ qb_object_new(PyTypeObject *to, char *name)
 
     return obj;
 }
-
+ 
 PyObject *
 qb_interface_get(qb_object_t *obj, char *name)
 {
@@ -50,11 +51,30 @@ qb_interface_get(qb_object_t *obj, char *name)
 static int
 _Py_object_init(qb_object_t *self, PyObject *args, PyObject *kwds)
 {
+    PyObject *cls, *name;
     PyObject *tuple = PyTuple_Pack(1, self);
 //    self->ob_dict = PyDict_New();
+    printf("self=%p t=%p\n", self, ((PyObject *)(&qb_interface_set_type))->ob_type);
     self->objset = PyObject_CallObject((PyObject *) &qb_object_set_type, NULL); //(PyObject *)self);
     self->ifaceset = PyObject_CallObject((PyObject *) &qb_interface_set_type, tuple); // (PyObject *)self);
     Py_DECREF(tuple);
+    cls = Py_TYPE(self);
+    printf("OBJINIT: self=%p cls=%p\n", self, cls);
+    name = PyUnicode_InternFromString("ifc");
+    printf("--%d\n", PyObject_SetAttr((PyObject *)self, name, self->ifaceset));
+/*
+    if (PyObject_HasAttr(cls, name)) {
+        Py_ssize_t i = 0;
+        PyObject *key, *value, *obj;
+        qb_interface_t *iface;
+        printf("cls has\n");
+        iface = Py_TYPE(PyObject_GetAttr((PyObject *)cls, name))->tp_dict;
+        while (PyDict_Next(iface, &i, &key, &value)) {
+            obj = PyObject_CallObject(iface, NULL); // PyTuple_Pack(1, self->parentobj));
+            printf("%d\n", obj); PyObject_SetAttr((PyObject *)Py_TYPE(self), key, obj);
+        }
+    }
+*/
 //    Py_INCREF(self->ob_dict);
 //    Py_INCREF(self->objset);
 //    Py_INCREF(self->ifaceset);
@@ -90,10 +110,35 @@ _Py_interface_traverse(qb_interface_t *self, visitproc visit, void *arg)
 static int
 _Py_interface_set_init(qb_interface_set_t *self, PyObject *args, PyObject *kwds)
 {
+    Py_ssize_t i;
+    PyObject *key, *value, *iface, *obj;
+
+    printf("init=%p\n", args);
+    if (args == NULL)
+        return 0;
+
     self->parentobj = PyTuple_GetItem(args, 0);
-//    self->iface = PyDict_New();
-//    Py_INCREF(self->iface);
+    fprintf(stderr, "interface_set_init: %p %p t=%p\n", self, self->iface_defs, self);
+    if (self->iface_defs == NULL)
+        return 0;
+
+    iface = self->iface_defs;
+    self->iface_defs = PyDict_New();
+    self->iface = iface;
+    i = 0;
+    printf("ifc=%p\n", self->iface);
+    while (PyDict_Next(self->iface, &i, &key, &value)) {
+        obj = PyObject_CallObject(iface, NULL); // PyTuple_Pack(1, self->parentobj));
+        printf("%d\n", obj); PyObject_GenericSetAttr((PyObject *)Py_TYPE(self), key, obj);
+    }
     return 0;
+}
+
+static PyObject *
+_Py_interface_set_getattr(qb_interface_set_t *self, PyObject *name)
+{
+    fprintf(stderr, "GETATTR: %p\n", self);
+    return Py_None;
 }
 
 static int
@@ -101,7 +146,10 @@ _Py_interface_set_setattr(qb_interface_set_t *self, PyObject *name, PyObject *if
 {
     PyObject *obj;
 
-    if (PyType_Check(iface)) {
+    printf("check=%d\n", Py_TYPE(self) == &qb_interface_set_type);
+
+#if 0
+    if (PyType_Check(iface) && !PyType_Check(self) && self->parentobj == NULL) {
         if (!PyObject_IsSubclass(iface, (PyObject *)&qb_interface_type)) {
             PyErr_SetString(PyExc_TypeError, "class should be subclass of `qb_interface_t'");
             return -1;
@@ -113,13 +161,21 @@ _Py_interface_set_setattr(qb_interface_set_t *self, PyObject *name, PyObject *if
             return -1;
         }
 #else
-        PyErr_SetString(PyExc_TypeError, "value must be class (subclass of `qb_interface_t')");
+        if (self->parentobj != NULL) {
+            PyErr_SetString(PyExc_TypeError, "it is not possible to change interface definition on instantiated class");
+        } else {
+            PyErr_SetString(PyExc_TypeError, "value must be class (subclass of `qb_interface_t')");
+        }
         return -1;
 #endif
     }
-    obj = PyObject_CallObject(iface, PyTuple_Pack(1, self->parentobj));
+#endif
+    //obj = PyObject_CallObject(iface, NULL);// PyTuple_Pack(1, self->parentobj));
+    if (self->iface_defs == NULL)
+        self->iface_defs = PyDict_New();
+    fprintf(stderr, "IFACESET: t=%p %p\n", Py_TYPE(self), self);
 
-    return PyObject_GenericSetAttr((PyObject *)self, name, obj);
+    return PyObject_GenericSetAttr((PyObject *)self, name, iface);
 }
 
 static int
@@ -134,19 +190,75 @@ _Py_object_set_init(qb_object_set_t *self, PyObject *args, PyObject *kwds)
 static int
 _Py_object_set_setattr(qb_object_set_t *self, PyObject *name, PyObject *item)
 {
+    fprintf(stderr, "OBJSET %p\n", self);
     PyObject_GenericSetAttr((PyObject *)self, name, item);
     return 0;
 }
 
+static PyObject *
+_Py_object_set_getattr(PyObject *self, PyObject *name)
+{
+    fprintf(stderr, "GGGG: %p\n", self);
+    return PyObject_GenericGetAttr(self, name);
+}
+
+static int
+_Py_object_set_descrset(PyObject *meth, PyObject *obj, PyObject *cls)
+{
+    fprintf(stderr, "SET %p\n", obj);
+    return 0;
+}
+
+static PyObject *
+_Py_interface_set_descrget(PyObject *meth, PyObject *obj, PyObject *cls)
+{
+    fprintf(stderr, "GET self=%p obj=%p cls=%p t=%s %s %p\n", meth, obj, cls, Py_TYPE(meth)->tp_name, ((PyTypeObject*)(cls))->tp_name, &qb_interface_set_type);
+    if (obj != NULL) {
+        PyObject *iface = ((qb_interface_set_t *)obj)->iface;
+        Py_INCREF(iface);
+        printf("---%p\n", iface);
+        return iface;
+    } else {
+        PyObject *name = PyUnicode_InternFromString("ifc");
+        Py_INCREF(name);
+        if (!PyObject_HasAttr(cls, name)) {
+            obj = PyObject_CallObject((PyObject *) &qb_interface_set_type, PyTuple_Pack(1, Py_None)); // (PyObject *)self);
+            printf("NIFAC: %p\n", obj);
+            printf("S--%p %d\n", cls, PyObject_GenericSetAttr(cls, name, obj));
+            printf("has=%p\n", _PyType_Lookup(cls, name));
+        } else {
+            obj = PyObject_GenericGetAttr(cls, name);
+        }
+        Py_DECREF(name);
+        printf("GIFACE: %p\n", obj);
+        return obj;
+    }
+    return meth;
+}
+
+static PyObject *
+_Py_interface_set_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
+{
+    PyObject *p;
+    p = PyType_GenericNew(type, args, kwds);
+    fprintf(stderr, "IFACE_NEW: t=%p a=%p o=%p\n", type, args, p);
+    return p;
+}
+
+static PyObject *
+_Py_interface_set_register(PyObject *self, PyObject *args)
+{
+    return self;
+}
+
 static PyMemberDef _Py_object_members[] = {
-    {"iface", T_OBJECT, offsetof(qb_object_t, ifaceset), READONLY},
+//    {"iface", T_OBJECT, offsetof(qb_object_t, ifaceset), READONLY},
     {"o", T_OBJECT, offsetof(qb_object_t, objset), READONLY},
     {NULL}
 };
 
-/*
 static PyMemberDef _Py_interface_members[] = {
-    {"parent", T_OBJECT, offsetof(qb_interface_t, parentobj), READONLY},
+    {"obj", T_OBJECT, offsetof(qb_interface_t, parentobj), READONLY},
     {NULL}
 };
 
@@ -155,16 +267,16 @@ static PyMethodDef _Py_interface_set_methods[] = {
         PyDoc_STR("register interface")},
     {NULL}
 };
-*/
 
 PyTypeObject qb_object_type = {
     .ob_base      = PyVarObject_HEAD_INIT(NULL, 0)
     .tp_name      = "qb_object_t",
     .tp_basicsize = sizeof(qb_object_t),
-    .tp_flags     = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_flags     = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE | Py_TPFLAGS_HEAPTYPE,
     .tp_doc       = PyDoc_STR("qb_object_t"),
-//    .tp_methods   = _Py_interface_set_methods,
     .tp_traverse  = (traverseproc)_Py_object_traverse,
+    .tp_descr_set = _Py_object_set_descrset,
+//    .tp_descr_get = _Py_object_set_descrget,
     .tp_members   = _Py_object_members,
     .tp_dictoffset = offsetof(qb_object_t, dict),
     .tp_init      = (initproc)_Py_object_init,
@@ -178,7 +290,9 @@ PyTypeObject qb_interface_type = {
     .tp_flags     = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
     .tp_doc       = PyDoc_STR("qb_interface_t"),
     .tp_traverse  = (traverseproc)_Py_interface_traverse,
-//    .tp_members   = _Py_interface_members,
+    .tp_members   = _Py_interface_members,
+    .tp_descr_set = _Py_object_set_descrset,
+//    .tp_descr_get = _Py_object_set_descrget,
     .tp_dictoffset = offsetof(qb_interface_t, dict),
     .tp_init      = (initproc)_Py_interface_init,
     .tp_new       = PyType_GenericNew,
@@ -191,6 +305,7 @@ PyTypeObject qb_object_set_type = {
     .tp_flags     = Py_TPFLAGS_DEFAULT,
     .tp_doc       = PyDoc_STR("qb_object_set_t"),
     .tp_setattro  = (setattrofunc)_Py_object_set_setattr,
+//    .tp_getattro  = (getattrofunc)_Py_object_set_getattr,
     .tp_dictoffset = offsetof(qb_object_set_t, o),
     .tp_init      = (initproc)_Py_object_set_init,
     .tp_new       = PyType_GenericNew,
@@ -200,12 +315,15 @@ PyTypeObject qb_interface_set_type = {
     .ob_base      = PyVarObject_HEAD_INIT(NULL, 0)
     .tp_name      = "qb_interface_set_t",
     .tp_basicsize = sizeof(qb_interface_set_t),
-    .tp_flags     = Py_TPFLAGS_DEFAULT,
+    .tp_flags     = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HEAPTYPE,
     .tp_doc       = PyDoc_STR("qb_inetrace_set_t"),
     .tp_setattro  = (setattrofunc)_Py_interface_set_setattr,
-    .tp_dictoffset = offsetof(qb_interface_set_t, iface),
+    .tp_getattro  = (getattrofunc)_Py_interface_set_getattr,
+    .tp_descr_get = _Py_interface_set_descrget,
+    .tp_dictoffset = offsetof(qb_interface_set_t, iface_defs),
     .tp_init      = (initproc)_Py_interface_set_init,
-    .tp_new       = PyType_GenericNew,
+    .tp_new       = _Py_interface_set_new,
+//    .tp_base      = &PyBaseObject_Type,
 };
 
 /* C API.
@@ -224,7 +342,7 @@ static struct PyModuleDef _core_module = {
 PyObject *
 PyInit__core(void)
 {
-    PyObject *m, *d, *x;
+    PyObject *m, *d, *x, *descr, *meth;
 
     if (PyType_Ready(&qb_interface_set_type) < 0)
         return NULL;
@@ -242,6 +360,17 @@ PyInit__core(void)
     if (m == NULL)
         return NULL;
 
+    descr = PyType_GenericAlloc(&qb_interface_set_type, 0);
+    printf("TYPE: t=%p desc=%p\n", Py_TYPE(descr), descr);
+    Py_INCREF(descr);
+    PyDict_SetItemString(qb_object_type.tp_dict, "iface", descr);
+
+/*
+    descr = PyDescr_NewMember(&qb_object_type, &_Py_object_members[0]);
+    printf("%d\n", PyDict_SetItemString(qb_object_type.tp_dict, "iface", descr));
+    meth = PyDescr_NewMethod(Py_TYPE(descr), &_Py_interface_set_methods[0]);
+    printf("%d\n", PyDict_SetItemString(Py_TYPE(descr)->tp_dict, "register", meth));
+*/
     /* Add some symbolic constants to the module */
     d = PyModule_GetDict(m);
     if (d == NULL)
