3464c2f194d869b4a35a6734033e1ae252b9c58b
Anton D. Kachalov <mouse@yandex.ru>
2015-01-06 11:18:45 +0200

    [lib/qb] Add `qb_pin_t' baseclass and `pin' decorator.
    Signed-off-by: Anton D. Kachalov <mouse@yandex.ru>

--- lib/qb/__init__.py
+++ lib/qb/__init__.py
@@ -1,5 +1,5 @@
 import sys
 import qb.rpc
 import qb.console
-from qb._core import qb_object_t, qb_interface_t, root
-from qb.decorators import interface
+from qb._core import qb_object_t, qb_interface_t, qb_pin_t, root
+from qb.decorators import interface, pin
--- lib/qb/decorators.py
+++ lib/qb/decorators.py
@@ -16,13 +16,25 @@
 # You should have received a copy of the GNU General Public License
 # along with this library.  If not, see <http://www.gnu.org/licenses/>.
 #
-from qb._core import qb_interface_t
+from qb._core import qb_interface_t, qb_pin_t
 
 def interface(name=None):
     def wrapper(klass):
         if not isinstance(klass, type) or not issubclass(klass, qb_interface_t):
             raise TypeError('value `{}\' have to be subclass of `qb_interface_t\''.format(klass))
         klass.__iface__ = name if isinstance(name, str) else klass.__name__
+        return klass
+
+    if isinstance(name, str) or name is None:
+        return wrapper
+
+    return wrapper(name)
+
+def pin(name=None):
+    def wrapper(klass):
+        if not isinstance(klass, type) or not issubclass(klass, qb_pin_t):
+            raise TypeError('value `{}\' have to be subclass of `qb_pin_t\''.format(klass))
+        klass.__pin__ = name if isinstance(name, str) else klass.__name__
         return klass
 
     if isinstance(name, str) or name is None:
